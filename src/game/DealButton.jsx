6dce187e46c3a1090220636ec4392cedf7d6df9c
christopher-brown1 <crouton_limpid.0g@icloud.com>
2025-11-04 16:14:49 -0700

    feat: enhance DealButton with play/pause functionality and integrate into Header

--- src/game/DealButton.jsx
+++ src/game/DealButton.jsx
@@ -1,15 +1,22 @@
 import style from "../game/dealButton.module.css"
 
 import play from "../game/assets/play.svg"
+import pause from "../game/assets/pause.svg"
 
-export const DealButton = () => {
+export const DealButton = ({ setIsDealing, isDealing }) => {
+
   return (
-    <div className={style.buttonContainer}>
+    <button
+      className={style.buttonContainer}
+      onClick={() => setIsDealing((v) => !v)}
+    >
       <div className={style.textContainer}>
         <h2 className={style.textBig}>Auto-Deal</h2>
-        <p className={style.textSmall}>Click to start dealing</p>
+        <p className={style.textSmall}>
+          {isDealing ? "Click to stop dealing" : "Click to start dealing"}
+        </p>
       </div>
-      <img src={play} />
-    </div>
+      <img src={isDealing ? pause : play} alt={isDealing ? "Pause" : "Play"} />
+    </button>
   )
 }
