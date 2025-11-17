b798be92d7fb6cdace38ceb2ff31ceb38a73ab9d
Jameson Brown <jamesonbrown@neolivity.com>
2025-11-17 00:53:22 -0700

    Update game state management and adjust deal interval
    - Increased DEAL_INTERVAL from 2000ms to 3000ms for better pacing.
    - Introduced initialGameState object to manage game state more effectively.
    - Added placeBet function to handle player betting logic within the game state.

--- src/logicEngine/consts.js
+++ src/logicEngine/consts.js
@@ -1,8 +1,22 @@
-export const DEAL_INTERVAL = 2000
+export const DEAL_INTERVAL = 3000
 export const DECK_API_URL = "https://deckofcardsapi.com/api/deck/"
 export const suitToPlayerMap = {
   HEARTS: "orange",
   DIAMONDS: "green",
   CLUBS: "blue",
   SPADES: "purple",
+}
+export const initialGameState = {
+  isOnboarding: false,
+  isBetting: false,
+  isDealing: false,
+  isRoundOver: false,
+  isGameOver: false,
+  cardsId: null,
+  roundCount: 0,
+  dangerPosition: 0,
+  dangerCards: [null, null, null, null, null, null, null],
+  history: ["dark", "dark", "dark", "dark", "dark"],
+  strikes: [], // array of colors
+  players: [],
 }
